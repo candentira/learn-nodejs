45e09978c470644ce0a0e561b92f5f295c5e4128
ViktoryiaKlimenka <Viktoryia_Klimenka@epam.com>
2020-09-07 10:54:40 +0300

    Task 3.1: create Users DB, populate it, Rest endpoints use Users DB via Sequelize ORM

--- task2-1.js
+++ task2-1.js
@@ -1,80 +1,87 @@
-import { USERS_LIST } from './constants';
+const { Sequelize, Op } = require('sequelize');
 import { validateSchema } from './validation';
 import { v4 as uuidv4 } from 'uuid';
 import { userUpdateSchema, userCreateSchema } from './users.schema';
+import User from './models/user';
 import express from 'express';
 const app = express();
 const port = 3000;
 
+const sequelize = new Sequelize('postgres://jypfzbwx:aYUoFzUvmkdbT98u4D0X1RPXkAlxBBQt@lallah.db.elephantsql.com:5432/jypfzbwx');
+
+sequelize.authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch(err => console.error('Unable to connect to the database:', err));
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
-
 app.get('/users', (req, res) => {
-    res.send(JSON.stringify(USERS_LIST));
+    User.findAll()
+        .then(users => res.send(JSON.stringify(users)))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'Couldn\'t retrieve Users' });
+        });
 });
 
 app.get('/users/:id', (req, res) => {
-    const user = USERS_LIST.find(item => item.id === req.params.id);
-
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404).json({ message: `User with id ${req.params.id} not found` });
-    }
+    User.findByPk(req.params.id)
+        .then(user => res.json(user))
+        .catch(err => {
+            console.error(err);
+            res.status(404).json({ message: `User with id ${req.params.id} not found` });
+        });
 });
 
 app.get('/getAutoSuggestUsers', (req, res) => {
     const { loginSubstring, limit } = req.query;
-    const filtered = USERS_LIST.filter(user => user.login.includes(loginSubstring))
-        .sort((a, b) => {
-            const loginA = a.login;
-            const loginB = b.login;
-            if (loginA < loginB) {
-                return -1;
-            }
-            if (loginA > loginB) {
-                return 1;
-            }
-            return 0;
+    User.findAll({ where: { login: { [Op.like]: `%${loginSubstring}%` } } })
+        .then(users => {
+            const filtered = users.sort((a, b) => {
+                const loginA = a.login;
+                const loginB = b.login;
+                if (loginA < loginB) {
+                    return -1;
+                }
+                if (loginA > loginB) {
+                    return 1;
+                }
+                return 0;
+            }).slice(0, limit);
+            res.json(filtered);
         })
-        .slice(0, limit);
-    res.json(filtered);
+        .catch(console.error);
 });
 
 app.post('/users', validateSchema(userCreateSchema), (req, res) => {
-    const user =  {
-        id: uuidv4(),
-        ...req.body,
-        isDeleted: false
-    };
-    USERS_LIST.push(user);
-    res.status(204).send();
+    User.create({ id: uuidv4(), ...req.body, isDeleted: false })
+        .then(() => res.status(204).send())
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'User wasn\'t created' });
+        });
 });
 
 app.put('/users/:id', validateSchema(userUpdateSchema), (req, res) => {
     const { id } = req.params;
-    const userIndex = USERS_LIST.findIndex(user => user.id === id);
-    if (userIndex >= 0) {
-        USERS_LIST[userIndex] = req.body;
-        res.status(204).send();
-    } else {
-        res.status(404).json({ message: `User with id ${id} not found` });
-    }
+    User.update(req.body, { where: { id } })
+        .then(() => res.status(204).send())
+        .catch(err => {
+            console.error(err);
+            res.status(404).json({ message: `User with id ${id} not found` });
+        });
 });
 
 app.delete('/users/:id', (req, res) => {
-    const user = USERS_LIST.find(item => item.id === req.params.id);
-    if (user) {
-        user.isDeleted = true;
-        res.status(200).json(user);
-    } else {
-        res.status(404).json({ message: `User with id ${req.params.id} was not deleted` });
-    }
+    const { id } = req.params;
+    User.update({ is_deleted: true }, { where: { id } })
+        .then(user => res.status(200).json(user))
+        .catch(err => {
+            console.log(err);
+            res.status(404).json({ message: `User with id ${req.params.id} was not deleted` });
+        });
 });
 
 app.listen(port, () => {
-    console.log(`Task 2-1 app listening at http://localhost:${port}`);
+    console.log(`Task 3-1 app listening at http://localhost:${port}`);
 });
